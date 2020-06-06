0d643dd23c43f5b15f5ceaadcc1f3e0ac3350fe2
Fabio da Silva <fabiodasilva95@outlook.com>
2020-06-06 20:32:38 -0300

    feat: adicionando funcionalidade ao app e construindo a interface.

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,8 +1,94 @@
-import React from 'react';
-import { View, ImageBackground, StyleSheet, Image, Text, } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import { View, StyleSheet, Image, Text, TouchableOpacity, SafeAreaView, Linking } from 'react-native';
+import { RectButton, } from 'react-native-gesture-handler';
+import * as MailComposer from 'expo-mail-composer';
+import api from '../../services/api';
+
+interface Params {
+  point_id: number;
+}
+
+interface Point {
+  point: {
+    image: string;
+    name: string;
+    email: string;
+    whatsapp: string;
+    city: string;
+    uf: string;
+  },
+  items: {
+    title: string;
+  }[];
+}
+
 const Detail = () => {
+  const [data, setData] = useState<Point>({} as Point);
+  const navigation = useNavigation();
+  const route = useRoute();
+
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => {
+      setData(response.data);
+    });
+  }, [])
+
+  function handleNavigationBack() {
+    navigation.goBack();
+  }
+
+  function handleComposeMail() {
+    MailComposer.composeAsync({
+      subject: "Interesse na Coleta de resíduos",
+      recipients: [data.point.email]
+    })
+  }
+
+  function handleWhatsapp() {
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre a coleta de resíduos`);
+  }
+
+  if (!data.point) {
+    return null;
+  }
+
   return (
-    <></>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigationBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+        <Image style={styles.pointImage} source={{
+          uri: data.point.image
+        }} />
+
+        <Text style={styles.pointName}>{data.point.name}</Text>
+        <Text style={styles.pointItems}>
+          {
+            data.items && data.items.map(item => item.title).join(', ')
+          }
+        </Text>
+
+        <View>
+          <Text style={styles.addressTitle}>Endereço</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf} </Text>
+        </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
+          <FontAwesome name="whatsapp" size={20} color="#fff" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
+          <Icon name="mail" size={20} color="#fff" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
+      </View>
+    </SafeAreaView>
   )
 }
 
@@ -10,7 +96,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     padding: 32,
-    paddingTop: 20,
+    paddingTop: 50,
   },
 
   pointImage: {
--- mobile/src/pages/Points/index.tsx
+++ mobile/src/pages/Points/index.tsx
@@ -1,8 +1,162 @@
-import React from 'react';
-import { View, ImageBackground, StyleSheet, Image, Text, } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { View, StyleSheet, Image, Text, TouchableOpacity, ScrollView, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri } from 'react-native-svg';
+import api from '../../services/api';
+import * as Location from 'expo-location';
+interface Item {
+  id: number;
+  title: string;
+  image: string;
+}
+
+interface Point {
+  id: number;
+  image: string;
+  name: string;
+  latitude: number;
+  longitude: number;
+}
+
+interface Params {
+  uf: string;
+  city: string;
+}
+
 const Points = () => {
+  const [items, setItems] = useState<Item[]>([]);
+  const [selectedItems, setSelectedItems] = useState<number[]>([]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0])
+  const [points, setPoints] = useState<Point[]>([]);
+  const route = useRoute();
+  const routeParams = route.params as Params;
+
+  const navigation = useNavigation();
+
+  function handleNavigationBack() {
+    navigation.goBack();
+  }
+
+  useEffect(() => {
+    api.get('points', {
+      params: {
+        city: routeParams.city,
+        uf: routeParams.uf,
+        items: selectedItems
+      }
+    }).then(response => {
+      setPoints(response.data)
+    })
+  }, [selectedItems])
+
+  useEffect(() => {
+    api.get("items").then(response => {
+      setItems(response.data)
+    })
+  }, [])
+
+  useEffect(() => {
+    async function loadPosition() {
+      const { status } = await Location.requestPermissionsAsync();
+
+      if (status !== 'granted') {
+        Alert.alert('Oooops...', 'Precisamos de sua permissão para obter a sua localização.')
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync();
+
+      const { latitude, longitude } = location.coords;
+
+      setInitialPosition([latitude, longitude]);
+    }
+
+    loadPosition();
+  }, [])
+
+  function handleNavigationToDetail(id: number) {
+    navigation.navigate('Detail', { point_id: id });
+  }
+
+  function handleSelectedItem(id: number) {
+    const alreadySelected = selectedItems.findIndex(item => item === id);
+
+    if (alreadySelected >= 0) {
+      const filteredItems = selectedItems.filter(item => item !== id);
+      setSelectedItems(filteredItems)
+    }
+    else {
+      setSelectedItems([...selectedItems, id])
+    }
+  }
+
   return (
-    <></>
+    <>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={handleNavigationBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+        </TouchableOpacity>
+
+        <Text style={styles.title}>Bem Vindo.</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta.</Text>
+
+        <View style={styles.mapContainer}>
+          {initialPosition[0] !== 0 && (
+            <MapView style={styles.map} initialRegion={{
+              latitude: initialPosition[0],
+              longitude: initialPosition[1],
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014
+            }}>
+              {
+                points && points.map(point => (
+                  <Marker key={String(point.id)} style={styles.mapMarker} onPress={() => handleNavigationToDetail(point.id)} coordinate={{
+                    latitude: point.latitude,
+                    longitude: point.longitude,
+                  }}>
+                    <View style={styles.mapMarkerContainer}>
+                      <Image style={styles.mapMarkerImage} source={{
+                        uri: point.image
+                      }} />
+                      <Text style={styles.mapMarkerTitle}>{point.name}</Text>
+                    </View>
+                  </Marker>
+                ))
+              }
+
+            </MapView>
+          )
+          }
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          {items && items.map(item => (
+            <TouchableOpacity
+              key={String(item.id)}
+              style={[
+                styles.item,
+                selectedItems.includes(item.id) ? styles.selectedItem : {},
+              ]}
+              onPress={() => handleSelectedItem(item.id)}
+              activeOpacity={0.6}
+            >
+              <SvgUri width={42} height={42} uri={item.image} />
+              <Text style={styles.itemTitle}>
+                {item.title}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </>
   )
 }
 
@@ -10,7 +164,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 32,
-    // paddingTop: 20 + Constants.statusBarHeight,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   title: {
