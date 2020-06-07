47322898b5645abf2d9341e59fe2e1b7cc7bf573
Fabio da Silva <fabiodasilva95@outlook.com>
2020-06-06 23:04:32 -0300

    fix: adicionado validação removendo pasta uploads e database

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Joi } from 'celebrate';
 import multer from 'multer';
 import multerConfig from './config/multer';
 import PointController from './controllers/PointsController';
@@ -10,7 +11,25 @@ const upload = multer(multerConfig);
 routes.get('/items', ItemsController.index);
 
 routes.get('/points', PointController.index)
-routes.post('/points', upload.single('image'), PointController.create);
 routes.get('/points/:id', PointController.show);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  PointController.create
+);
 
 export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import cors from 'cors';
 import routes from './routes';
 import { resolve } from 'path';
+import { errors } from 'celebrate';
+
 const app = express();
 
 app.use(cors());
@@ -11,5 +13,6 @@ app.use(routes);
 
 app.use("/uploads", express.static(resolve(__dirname, '..', 'uploads')))
 
+app.use(errors());
 
 app.listen(3333, () => console.log("✔ Server started!"))
